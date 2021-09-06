69e40c34831dd058db553576add44f1fd034e2c3
GabrielNM12 <88680811+GabrielNM12@users.noreply.github.com>
2021-09-06 16:28:17 -0300

    Ex05

---
+++ Ex05.py
@@ -0,0 +1,22 @@
+Filmes      =   []
+Jogos       =   []
+Livros      =   []
+Esportes    =   []
+
+Filmes.append("Alvin e os Esquilos")
+Filmes.append("O Homem nas Trevas 2")
+
+Jogos.append("Dark Souls")
+Jogos.append("Bloodborne")
+
+Livros.append("Espadachim de Carvão")
+Livros.append("Diário de Anne Frank")
+
+Esportes.append("Futebol")
+Esportes.append("Basquete")
+
+Listona =   ["Filmes","Jogos","Livros"," Esportes"]
+
+print(Livros[0])
+del Esportes[1]
+Listona.append("Disciplina")
