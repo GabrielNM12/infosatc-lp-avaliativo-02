349fad1cc37964ffd819f6436e1882a78351906d
GabrielNM12 <88680811+GabrielNM12@users.noreply.github.com>
2021-09-06 16:28:30 -0300

    Ex06

---
+++ Ex06.py
@@ -0,0 +1,5 @@
+Carrinho    =   ["GeForce RTX 2080Ti","gabinete  cooler  master", "Memória RAM 16GB", "SSD 1T", "Processador i8 dual core"]
+print(Carrinho)
+del Carrinho[0]
+del Carrinho[0]
+print(Carrinho)
